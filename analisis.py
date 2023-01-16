37525dde433e7c2995d1deae07588483291d2ef3
OEM Configuration (temporary user) <jorgeandresbv1@gmail.com>
2023-01-16 09:59:49 -0600

    Another way for processing data
    dowload process data to csv and remove the .nc, also added new details to the notebook

--- analisis.py
+++ analisis.py
@@ -4,55 +4,48 @@ import subprocess
 import pandas as pd
 import sys
 
-day = sys.argv[1]
-year = sys.argv[2]
-
+file = sys.argv[1]
 
 #### MAIN  ####
 
-file_output = subprocess.check_output(f'ls data_{day}_{year} | grep OR', shell = True).decode('utf-8')
-file_list = file_output.split('\n')
-file_list.remove('')
+try:
+    df = pd.read_csv('data_frame.csv')
+except FileNotFoundError:
+    df = pd.DataFrame({'date':[], 'pressure': [], 'wind_speed': [], 'wind_direction': [], 'lat': [], 'lon': []})
+
+### Obtaining data
+data = Dataset(file)
+
+wind_speed = data.variables['wind_speed'][:]
+wind_direction = data.variables['wind_direction'][:]
+lat = data.variables['lat'][:]
+lon = data.variables['lon'][:]
+pressure = data.variables['pressure'][:]
+
+#date
+output = subprocess.check_output(f'ncinfo {file} | grep time_coverage_end', shell=True).decode('utf-8')
+date = pd.to_datetime(output.replace('    time_coverage_end: ','').replace('Z\n',''),format='%Y-%m-%dT%H:%M:%S')
 
 
+#### Limits
 
-df = pd.DataFrame({'time':[], 'pressure': [], 'wind_speed': [], 'wind_direction': [], 'lat': [], 'lon': []})
+# Guanacaste límites 9.55 - 11.21,-85.90 - -84.90 --> Provincia entera
+# Costa Rica -86.2 - -82.3 y 7.9 - 11.3
+guana_lat = (7.9<lat)*(lat<11.3)#--> 10.2 - 10.6 Zona plana 
+guana_lon = (-86.2<lon)*(lon<-82.3)#--> -85.6 - -85.15 Zona plana 
+guana_pre = (700<pressure)
+guanacaste = guana_lat*guana_lon*guana_pre
 
-for file in file_list:
-    file = f'data_{day}_{year}/{file}'
-    ### Obtaining data
-    data = Dataset(file)
+#Mover a un dataframe pequeño
 
-    wind_speed = data.variables['wind_speed'][:]
-    wind_direction = data.variables['wind_direction'][:]
-    lat = data.variables['lat'][:]
-    lon = data.variables['lon'][:]
-    pressure = data.variables['pressure'][:]
-    
-    #date
-    output = subprocess.check_output(f'ncinfo {file} | grep time_coverage_end', shell=True).decode('utf-8')
-    date = pd.to_datetime(output.replace('    time_coverage_end: ','').replace('Z\n',''),format='%Y-%m-%dT%H:%M:%S')
+for i in range(len(wind_speed[guanacaste])):
+    if wind_direction[guanacaste][i] != '--':
+        df_i = pd.DataFrame({'date':[date],
+        'pressure': [pressure[guanacaste][i]],
+        'wind_speed': [wind_speed[guanacaste][i]],
+        'wind_direction': [wind_direction[guanacaste][i]],
+        'lat': [lat[guanacaste][i]],
+        'lon': [lon[guanacaste][i]]})
+        df = pd.concat([df, df_i],ignore_index=True)
 
-
-    #### Limits
-
-    # Guanacaste límites 9.55 - 11.21,-85.90 - -84.90 --> Provincia entera
-    # Costa Rica -86.2 - -82.3 y 7.9 - 11.3
-    guana_lat = (7.9<lat)*(lat<11.3)#--> 10.2 - 10.6 Zona plana 
-    guana_lon = (-86.2<lon)*(lon<-82.3)#--> -85.6 - -85.15 Zona plana 
-    guana_pre = (700<pressure)
-    guanacaste = guana_lat*guana_lon*guana_pre
-
-    #Mover a un dataframe pequeño
-    
-    for i in range(len(wind_speed[guanacaste])):
-        if wind_direction[guanacaste][i] != '--':
-            df_i = pd.DataFrame({'time':[date],
-            'pressure': [pressure[guanacaste][i]],
-            'wind_speed': [wind_speed[guanacaste][i]],
-            'wind_direction': [wind_direction[guanacaste][i]],
-            'lat': [lat[guanacaste][i]],
-            'lon': [lon[guanacaste][i]]})
-            df = pd.concat([df, df_i],ignore_index=True)
-
-df.to_csv(f'data_{day}_{year}/data_frame.csv')
+df.to_csv(f'data_frame.csv', index=False)
--- data.bash
+++ data.bash
@@ -1,10 +1,12 @@
 #!/bin/bash
-
-for hour in {00..23}
+rm -r data_$1_$2
+mkdir data_$1_$2
+cd data_$1_$2
+for hour in {13..23}
 do
 	filename=$(aws s3 ls --no-sign-request s3://noaa-goes16/ABI-L2-DMWF/${2}/${1}/${hour}/ | tr -s ' ' | cut -d' ' -f4 | grep C02)
-	echo ${filename}
-	wget https://noaa-goes16.s3.amazonaws.com/ABI-L2-DMWF/$2/$1/${hour}/${filename} -P data_$1_$2
+	wget https://noaa-goes16.s3.amazonaws.com/ABI-L2-DMWF/$2/$1/${hour}/${filename}
+	python3 ../analisis.py ${filename}
+	rm ${filename}
 done
-
-python3 analisis.py $1 $2
+cd ..
